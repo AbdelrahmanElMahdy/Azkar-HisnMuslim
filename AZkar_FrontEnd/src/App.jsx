1622e87d9af403a8441082c71ac3004f0eea1a1d
MHDI3 <a.b.mahdey@gmail.com>
2021-05-03 16:55:58 +0200

    adding dependecies comment:         1- fontawesome         2- bootstrap

--- AZkar_FrontEnd/src/App.jsx
+++ AZkar_FrontEnd/src/App.jsx
@@ -1,8 +1,13 @@
-import './App.css';
 import {Route} from 'react-router-dom'
+
+import './App.css';
+
+import NavBar from './NavBar'
+
 function App() {
   return (
     <div className="App">
+       <Route path="/" component={NavBar}/>
        <Route path="/index"    component= { props =>{ return (<div>index</div> )}} />
        <Route path="/profile"  component= { props =>{ return (<div>Profile</div>  )}} />
        <Route path="/home"     component= { props =>{ return (<div>Home</div>  )}}  />
--- AZkar_FrontEnd/src/index.jsx
+++ AZkar_FrontEnd/src/index.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import {BrowserRouter} from 'react-router-dom'
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(
   
