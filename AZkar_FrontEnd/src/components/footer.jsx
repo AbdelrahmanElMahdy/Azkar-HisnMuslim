e86f3e067f0f6fce2eb3dcb0780e872de606d92e
MHDI3 <a.b.mahdey@gmail.com>
2021-05-04 06:15:58 +0200

    create footer component

---
+++ AZkar_FrontEnd/src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+class Footer extends Component {
+    state = {  }
+    render() { 
+        return (
+           
+              <div className="modfooter">© 2021 Copyright
+              </div>
+            
+           
+        );
+    }
+}
+ 
+export default Footer;
