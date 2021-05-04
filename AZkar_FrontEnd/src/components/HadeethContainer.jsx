5a08845f103c656249f4ce8243b332408165446b
MHDI3 <a.b.mahdey@gmail.com>
2021-05-04 15:10:24 +0200

    modify hadeeth container comment:         make hadeeth word the the center of the page and responsive

--- AZkar_FrontEnd/src/components/HadeethContainer.jsx
+++ AZkar_FrontEnd/src/components/HadeethContainer.jsx
@@ -3,13 +3,146 @@ class HadeethContainer extends Component {
     state = {  }
     render() { 
         return (
-<div className="row h-50 mt-50">
-  <div className="col-sm-12 h-100 d-table mt-50">
-    <div className="card card-block d-table-cell align-middle mt-50">
-    I am groot
-    </div>
-  </div>
-</div>
+          <div className="Centered">
+           
+           <div className="container-with-centered-content">
+
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+            hello world
+           </div>
+          </div>
           );
 }
 }
