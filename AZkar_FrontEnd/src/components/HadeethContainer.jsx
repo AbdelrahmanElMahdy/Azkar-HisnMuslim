02ee072f7f7101f4249287a4b31281ab44ea0b08
MHDI3 <a.b.mahdey@gmail.com>
2021-05-19 05:34:01 +0200

    editing hadeeth container

--- AZkar_FrontEnd/src/components/HadeethContainer.jsx
+++ AZkar_FrontEnd/src/components/HadeethContainer.jsx
@@ -1,17 +1,13 @@
-import {Component} from 'react'
-class HadeethContainer extends Component {
-    state = {  }
-    render() { 
+const HadeethContainer= (props) => {
         return (
           <div className="Centered">
            <div className="container-with-centered-content">
-              <div>hadeeth container</div>
-           </div>
-           <button className="btn btn-primary">Start</button>
+              <div>{props.hadeethText}</div>
+            </div>
           </div>
           );
 }
-}
+
 
  
 export default HadeethContainer;
